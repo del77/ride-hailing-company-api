63ee22b878a776b755f7ac80e5da2d23f72ddc7c
del <jakubprzybylski@hotmail.com>
2020-08-12 23:12:14 +0200

    split rides controller

---
+++ Api/Controllers/CustomersController.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Application.Rides.Command;
+using Application.Rides.Queries;
+using Core.Domain;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Authorize(Roles = UserRoles.Customer, AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class CustomersController : ApiController
+    {
+        [HttpGet("{rideId}")]
+        public async Task<IActionResult> GetById(GetRideQuery query)
+        {
+            var ride = Mediator.Send(query);
+            
+            return Ok();
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> OrderRide(OrderRideCommand command)
+        {
+            var id = await Mediator.Send(command);
+            
+            return CreatedAtAction(nameof(GetById), id);
+        }
+        
+        [HttpPatch("rides/{rideId}/cancel")]
+        public async Task<IActionResult> CancelRequestedRide(CancelRequestedRideCommand command)
+        {
+            await Mediator.Send(command);
+
+            return Ok();
+        }
+    }
+}
---
+++ Api/Controllers/DriversController.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using Application.Rides.Command;
+using Application.Rides.Queries;
+using Core.Domain;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Authorize(Roles = UserRoles.Driver, AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class DriversController : ApiController
+    {
+        [HttpGet("rides/requested")]
+        public async Task<IActionResult> GetRequestedRides()
+        {
+            var rides = await Mediator.Send(new GetRequestedRidesQuery());
+            
+            return Ok(rides);
+        }
+        
+        [HttpPatch("rides/{rideId}/pick-up-ride")]
+        public async Task<IActionResult> PickUpRide(PickUpRideCommand command)
+        {
+            await Mediator.Send(command);
+
+            return Ok();
+        }
+
+        [HttpPatch("rides/{rideId}/start-ride")]
+        public async Task<IActionResult> StartRide(StartRideCommand command)
+        {
+            await Mediator.Send(command);
+
+            return Ok();
+        }
+
+        [HttpPatch("rides/{rideId}/finish")]
+        public async Task<IActionResult> FinishRide(FinishRideCommand command)
+        {
+            await Mediator.Send(command);
+
+            return Ok();
+        }
+    }
+}
